e8f6a072d2c2c5451a52a160ce03e0b4769b21bd
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2017-01-14 16:12:21 +0100

    Initialized npm package Created a package that will be pushed to the npm registry. It has some basic filtering capabilities.
    Resolves #1

---
+++ nukat.js
@@ -0,0 +1,37 @@
+const nukat = require('./nukta.json')
+
+/**
+ * Search for jokes
+ * @param {RegExp} pattern Regular expression to look for
+ */
+const search = (pattern) => nukat.filter(joke => new RegExp(pattern).test(joke.joke))
+
+/**
+ * Search for jokes in a specific language
+ * @param {string} lang language
+ */
+const filterByLang = (lang = 'ar') => nukat.filter(joke => joke.lang === lang)
+
+/**
+ * Search for jokes with some specific tags
+ * @param {string|Array} tag An Array or comma separated string of tags
+ * @return {Array}
+ */
+const filterByTag = (tag) => {
+  if (Array.isArray(tag))
+    tag = tag.join('|')
+  else if (typeof tag === 'string')
+    tag = tag.replace(',', '|')
+  else
+    throw new TypeError('Expected string|Array<string>')
+
+  return nukat.filter(joke => new RegExp(tag).test(joke.tags))
+}
+
+
+module.exports = {
+  nukat,
+  search,
+  filterByLang,
+  filterByTag,
+}
